356d47e87f290df8282121c8b5bd84b4bebbf51d
chetan <chetan9chaudhari@gmail.com>
2021-01-27 22:14:28 +0530

    Postgre sql

--- events/models.py
+++ events/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+	event_image = models.ImageField(upload_to='events_images/')
+	event_char = models.CharField(max_length=300)
